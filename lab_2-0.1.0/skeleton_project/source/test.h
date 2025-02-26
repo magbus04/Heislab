355a39d4380f72495c6a4331c2c22ae2332609c6
Anneli Stokke <annelistokke@hotmail.com>
2025-02-26 14:58:42 +0100

    legger til testfil med utvided kode fra forrige uke. prøvd å følg FAT acceptance greian

---
+++ lab_2-0.1.0/skeleton_project/source/test.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "elevio.h"
+
+#define DOOR_OPEN_TIME 3            // Døråpentid: 3 sekunder
+#define SLEEP_NS (20 * 1000 * 1000)   // 20 ms pause i loopen
+
+// Globale variabler for bestillinger og kalibrering
+bool orders[N_FLOORS][N_BUTTONS] = { { false } };
+bool calibrated = false;
+
+// Definerer heisens tilstander
+typedef enum {
+    STATE_CALIBRATING,
+    STATE_IDLE,
+    STATE_MOVING,
+    STATE_DOOR_OPEN,
+    STATE_EMERGENCY_STOP
+} ElevatorState;
+
+ElevatorState state = STATE_CALIBRATING;
+MotorDirection currentDirection = DIRN_STOP;
+
+// Funksjonsprototyper
+void calibrateElevator(void);
+void updateOrders(void);
+bool ordersAtFloor(int floor);
+void clearOrdersAtFloor(int floor);
+void clearAllOrders(void);
+MotorDirection chooseDirection(int currentFloor);
+void openDoor(void);
